469a77526395759ef922ef032d0ffb4d0baab6c2
tashiwangchuk619@gmail.com <tashiwangchuk619@gmail.com>
2024-01-05 06:19:16 +0530

    Created a project

---
+++ src/test/java/com/internship/usermanagementsystem/UserManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internship.usermanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
